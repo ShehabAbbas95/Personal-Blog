8973074d789291ec0b10184465464f0a9914bf3f
Shehab <shehababbas2019@gmail.com>
2020-08-30 19:15:00 +0200

    comments section has fineshed (think about live comments system i.e websocket)

--- blog/static/blog/js/post-detail.js
+++ blog/static/blog/js/post-detail.js
@@ -4,7 +4,6 @@ $('#commentForm').on('submit', function(e){
 $.ajax({
 method:'POST',
 url:'blog/post/<int:id>/likepost/',
-
 data:{
   // Get the data to pass it to the server-side (calling it in python)
 comment:$('#comment').val(),
@@ -20,26 +19,7 @@ success: function(lastcomment) {
     $('#livecomment-text').html(lastcomment.comment);
     $('#lastcomment-date').html('Just Now');
     console.log(lastcomment)
-
-
-
 },
 });
 });
 });
-
-
-// function posting(e){
-//   $.ajax({
-//     method:'POST',
-//     url:'blog/post/<int:id>/likepost/',
-//     data:
-//     const comment = document.querySelector('#comment').value();
-//     const username = document.querySelector('#visitor_name').value();
-//     csrfmiddlewaretoken = $('input[name=csrfmiddlewaretoken]').value(),
-//     success: function(data) {
-//         alert('Comment Submmited Successfully');
-//          e.preventDefault();
-//     },
-//   })
-// }
